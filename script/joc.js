1263357ea622e61216f70965d891fc0f276cb46a
danisancheziticbcn <2023_daniel.sanchez@iticbcn.cat>
2024-10-07 20:50:01 +0200

    capçaleraarreglada

--- script/joc.js
+++ script/joc.js
@@ -1,35 +1,36 @@
-//Definim tots els objectes
+// Definim tots els objectes
 const inpuObj = document.getElementById("paraulaSecreta");
 const buttObj = document.getElementById("button");
 
-//Variables
+// Variables
 let paraulaSecreta;
 
-function comencarPartida(){
+function comencarPartida() {
 
-    paraulaSecreta = inpuObj.value
+    paraulaSecreta = inpuObj.value;
 
-    if(paraulaSecreta){
-        if(paraulaSecreta.type="Number"){
-            alert("Has d'introduir una paraula, un número no és valid")
-        }else{
-            if(paraulaSecreta.length > 3){
-            console.log(paraulaSecreta)
-            console.log(paraulaSecreta.split(""))
-            inpuObj.disabled = true;
-            buttObj.disabled = true;
-            } else{
-            alert("Has d'introduir una paraula de com a mínim 4 lletres")
+    if (paraulaSecreta) {
+        if (!isNaN(paraulaSecreta)) {
+            alert("Has d'introduir una paraula, un número no és vàlid");
+        } else {
+            if (paraulaSecreta.length > 3) {
+                console.log(paraulaSecreta);
+                console.log(paraulaSecreta.split(""));
+                inpuObj.disabled = true;
+                buttObj.disabled = true;
+            } else {
+                alert("Has d'introduir una paraula de com a mínim 4 lletres");
             }
         }
-    }else{
-    alert("No has introduit una paraula")
+    } else {
+        alert("No has introduït una paraula");
     }
 }
-function mostrarParaula(){
-    if(inpuObj.type="password"){
-        inpuObj.type="text";
-    } else{
-        inpuObj.type="password";
+
+function mostrarParaula() {
+    if (inpuObj.type === "password") {
+        inpuObj.type = "text";
+    } else {
+        inpuObj.type = "password";
     }
 }
